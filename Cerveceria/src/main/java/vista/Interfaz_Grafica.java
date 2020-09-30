1a1928b68cd7dea8382463d4a34552c505261c9c
lautalaserna <lasernalauta@gmail.com>
2020-09-30 16:15:48 -0300

    UI

--- Cerveceria/src/main/java/vista/Interfaz_Grafica.java
+++ Cerveceria/src/main/java/vista/Interfaz_Grafica.java
@@ -6,6 +6,13 @@ import negocio.Negocio;
 public class Interfaz_Grafica {
 	Negocio negocio = new Negocio();
 
+	/**
+	 * Metodo que transforma un string a integer.
+	 * 
+	 * @param entrada Un string que debe ser un numero.
+	 * @return Integer con el valor del numero.
+	 * @throws NoEnteroException Si se ingresa cualquier otro caracter.
+	 */
 	private int leerString(String entrada) throws NoEnteroException {
 		int salida;
 		try {
@@ -14,13 +21,18 @@ public class Interfaz_Grafica {
 			throw new NoEnteroException("No es un numero");
 		}
 		return salida;
-
 	}
 
+	/**
+	 * Metodo interno que verifica que siempre se cumpla la condicion invariante.
+	 */
 	private void verificarInvariante() {
 		assert this.negocio != null : "No existe el negocio";
 	}
 
+	/**
+	 * Metodo que simula la lectura del numero de mesas para abrir el local.
+	 */
 	public void abrirLocal() {
 		try {
 			int nroMesas = leerString("10");
@@ -31,11 +43,29 @@ public class Interfaz_Grafica {
 		verificarInvariante();
 	}
 
+	/**
+	 * Metodo que simula la lectura de una mesa para abrir.
+	 */
 	public void ocuparMesa() {
-
+		try {
+			int nroMesa = leerString("3");
+		} catch (NoEnteroException e) {
+			System.out.println(e.getMessage());
+		}
+		//this.negocio.ocuparMesa(nroMesa);
+		verificarInvariante();
 	}
 
+	/**
+	 * Metodo que simula la lectura de una mesa para cerrar.
+	 */
 	public void cerrarMesa() {
-
+		try {
+			int nroMesa = leerString("3");
+		} catch (NoEnteroException e) {
+			System.out.println(e.getMessage());
+		}
+		//this.negocio.cerrarMesa(nroMesa);
+		verificarInvariante();
 	}
 }
